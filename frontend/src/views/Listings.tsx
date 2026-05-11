0176305bc56f99b3eb1a16a8be4119be3f80c576
DemirMasic <demir.masic@stu.ibu.edu.ba>
2026-05-11 19:04:16 +0200

    fixed responsivness of the listing page

--- frontend/src/views/Listings.tsx
+++ frontend/src/views/Listings.tsx
@@ -25,7 +25,7 @@ function Listings() {
 
   return (
     <div className="mx-auto justify-center flex flex-col w-full min-h-screen bg-slate-100 px-4 py-8 md:px-8">
-      <div className="mx-auto w-full max-w-[1258.56px] mb-8">
+      <div className="mx-auto w-full max-w-368 mb-8">
         <h1 className="text-3xl font-bold text-slate-900">Listings</h1>
         <p className="mt-2 text-sm text-slate-600">
           Browse all available listings in the marketplace.
@@ -36,7 +36,7 @@ function Listings() {
           <Categories isListingsPage></Categories>
         </div>
         {listings.length > 0 && (
-        <div className="items-start auto-rows-min grid max-w-6xl grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div className="items-start w-full auto-rows-min grid max-w-6xl grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {listings.map((listing) => (
             <ListingCard key={listing.id} listing={listing} />
           ))}
@@ -45,7 +45,7 @@ function Listings() {
 
 
         {listings.length === 0 && (
-          <div className="mt-10 max-w-md rounded-2xl border border-dashed border-slate-300 bg-white p-8 text-center text-slate-500">
+          <div className="w-full mt-10 max-w-6xl rounded-2xl border border-dashed border-slate-300 bg-white p-8 text-center text-slate-500">
             No listings found.
           </div>
         )}
