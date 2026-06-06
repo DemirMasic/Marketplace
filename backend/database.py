a5f382cfcaae47c87fe3b4e7870f805a95356f23
DemirMasic <demir.masic@stu.ibu.edu.ba>
2026-06-06 12:41:07 +0200

    Minor fix

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:kornjaca321%40@market.chessomania.art:5432/postgrestest"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
